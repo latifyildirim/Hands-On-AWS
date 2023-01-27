62b554601298b3d275f42b7e9f89bf55f132c881
latif <lyildirim.3017@gmail.com>
2023-01-27 23:43:04 +0100

    .

--- 2023_01_27_MariaDB/MariaDB-Part-4.sh
+++ 2023_01_27_MariaDB/MariaDB-Part-4.sh
@@ -13,7 +13,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install the mariadb-client.
-sudo apt-get install mariadb-client -y
+        sudo apt-get install mariadb-client -y
 
 # Connect the latifdb on MariaDB Server on the other EC2 instance (pw:latif1234).
 mysql -h ec2-3-94-163-77.compute-1.amazonaws.com -u latifuser -p
